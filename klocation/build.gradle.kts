91f1cddbb69119214b974a51681145bf16470798
Henry Addo <henry@addhen.org>
2024-08-02 06:21:28 -0700

    Clean build for library module

--- klocation/build.gradle.kts
+++ klocation/build.gradle.kts
@@ -1,45 +1,41 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
   id("convention.plugin.android.library")
   id("convention.plugin.kotlin.multiplatform")
-  id("convention.plugin.metalava")
+  id("org.jetbrains.dokka")
   id("convention.plugin.maven.publication")
+  id("convention.plugin.metalava")
 }
 
 kotlin {
-    jvm()
-    androidTarget {
-        publishLibraryVariants("release")
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-        }
+  sourceSets {
+    commonMain {
+      dependencies {
+        //put your multiplatform dependencies here
+      }
     }
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-    linuxX64()
 
-    sourceSets {
-        commonMain {
-            dependencies {
-                //put your multiplatform dependencies here
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(libs.kotlin.test)
-            }
-        }
+    androidMain {
+      dependencies {
+      }
     }
+
+    val commonTest by getting {
+      dependencies {
+        implementation(libs.kotlin.test)
+      }
+    }
+  }
 }
 
 android {
-    namespace = "com.addhen.klocation"
-    compileSdk = libs.versions.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
+  namespace = "com.addhen.klocation"
+  defaultConfig {
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  testOptions {
+    unitTests {
+      isIncludeAndroidResources = true
     }
+  }
 }
