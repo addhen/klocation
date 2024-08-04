c9cebe6cdda69d3edac8a20936935d5a09798d49
Henry Addo <henry@addhen.org>
2024-08-03 21:38:06 -0700

    Add necessary JVM and compiler warning surpression warning

--- convention-plugins/src/main/kotlin/com/addhen/gradle/convention/Kotlin.kt
+++ convention-plugins/src/main/kotlin/com/addhen/gradle/convention/Kotlin.kt
@@ -15,10 +15,28 @@ fun Project.configureKotlin() {
   tasks.withType<KotlinCompilationTask<*>>().configureEach {
     compilerOptions {
       allWarningsAsErrors.set(true)
-
+      freeCompilerArgs.add(
+        // expect/actual classes (including interfaces, objects, annotations, enums, actual typealiases) in Beta
+        // https://youtrack.jetbrains.com/issue/KT-61573
+        "-Xexpect-actual-classes"
+      )
       if (this is KotlinJvmCompilerOptions) {
         // Target JVM 11 bytecode
         jvmTarget.set(JvmTarget.JVM_11)
+        freeCompilerArgs.addAll(
+          "-Xjsr305=strict",
+          // Match JVM assertion behavior:
+          // https://publicobject.com/2019/11/18/kotlins-assert-is-not-like-javas-assert/
+          "-Xassertions=jvm",
+          // Potentially useful for static analysis tools or annotation processors.
+          "-Xemit-jvm-type-annotations",
+          // Enable new jvm-default behavior
+          // https://blog.jetbrains.com/kotlin/2020/07/kotlin-1-4-m3-generating-default-methods-in-interfaces/
+          "-Xjvm-default=all",
+          // https://kotlinlang.org/docs/whatsnew1520.html#support-for-jspecify-nullness-annotations
+          "-Xtype-enhancement-improvements-strict-mode",
+          "-Xjspecify-annotations=strict",
+        )
       }
     }
   }
