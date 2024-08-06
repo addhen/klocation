b4745356359eb1fa6563bc1af07197c02574ef43
Henry Addo <henry@addhen.org>
2024-08-06 16:07:14 -0700

    Add documentation for the LocationService class

--- klocation/src/androidMain/kotlin/com/addhen/klocation/LocationService.kt
+++ klocation/src/androidMain/kotlin/com/addhen/klocation/LocationService.kt
@@ -5,15 +5,34 @@ package com.addhen.klocation
 import android.content.Context
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * This class provides methods to observe location updates, retrieve the last known location,
+ * and stop location tracking. It uses a [LocationProvider] to handle the actual location operations.
+ *
+ * @property context The Android [Context] to be used to get the [LocationManager] and for
+ *                   checking for the necessary required permissions.
+ * @property locationProvider The [LocationProvider] used to manage location updates.
+ *                            Defaults to [AndroidLocationProvider].
+ */
 actual class LocationService(
   private val context: Context,
   actual val locationProvider: LocationProvider = AndroidLocationProvider(context),
 ) {
 
+  /**
+   * Observes location updates as a [Flow] of [LocationState].
+   *
+   * @return A [Flow] emitting [LocationState] representing location updates or other states.
+   */
   actual fun observeLocationUpdates(): Flow<LocationState> {
     return locationProvider.observeLocationUpdates()
   }
 
+  /**
+   * Retrieves the last known location.
+   *
+   * @return A [LocationState] representing the last known location or other states.
+   */
   actual suspend fun getLastKnownLocation(): LocationState {
     return locationProvider.getLastKnownLocation()
   }
