e4d21875622cfb77a2bc38cc26179245d0a7fb6d
Henry Addo <henry@addhen.org>
2024-08-07 11:07:33 -0700

    Add compose State support for observing device location and get last known location

---
+++ klocation-compose/src/commonMain/kotlin/com/addhen/klocation/compose/KlocationCompose.kt
@@ -0,0 +1,24 @@
+package com.addhen.klocation.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.produceState
+import com.addhen.klocation.LocationService
+import com.addhen.klocation.LocationState
+import kotlinx.coroutines.flow.distinctUntilChanged
+
+@Composable
+fun locationUpdatesState(locationService: LocationService): State<LocationState> {
+  return locationService
+    .observeLocationUpdates()
+    .distinctUntilChanged()
+    .collectAsState(LocationState.CurrentLocation(null))
+}
+
+@Composable
+fun lastKnowLocationState(locationService: LocationService): State<LocationState> {
+  return produceState(LocationState.CurrentLocation(null)) {
+    locationService.getLastKnownLocation()
+  }
+}
